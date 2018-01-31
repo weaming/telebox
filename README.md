135da282eabade7316b23029c8fa1e3d972ec894
weaming <garden.yuen@gmail.com>
2018-01-31 10:31:42 +0800

    ok

---
+++ README.md
@@ -0,0 +1,11 @@
+Telegram bot act as a box receiving messsages via HTTP
+
+## OpenSSL missing headers
+
+    sudo apt-get install pkg-config libssl-dev
+
+## Run
+
+1. Set telegram bot token in environment as `TELEGRAM_BOT_TOKEN`
+1. Run executable
+1. Send data in body via HTTP to `0.0.0.0:12345`
--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,79 @@
-extern crate futures;
-extern crate telegram_bot;
-extern crate tokio_core;
+extern crate reqwest;
+extern crate hyper;
 
 use std::env;
+use std::thread;
+use std::io::Read;
+use std::sync::{Mutex};
+use std::sync::mpsc::{channel, Sender, Receiver};
 
-use futures::Stream;
-use tokio_core::reactor::Core;
-use telegram_bot::*;
+use hyper::server::{Handler, Server, Request, Response};
+
+struct Inbox{
+    inbox: Mutex<Sender<String>>
+}
+
+impl Handler for Inbox {
+    fn handle(&self, mut req: Request, res: Response) {
+        let mut msg = String::new();
+        req.read_to_string(&mut msg).unwrap();
+        println!("{}: {}", req.remote_addr, msg);
+        self.inbox.lock().unwrap().send(format!("{} \n\n{}", msg, req.remote_addr)).unwrap();
+        res.send(b"OK\n").unwrap();
+    }
+}
+
+fn inbox(tx: Sender<String>) {
+    Server::http("0.0.0.0:12345").unwrap().handle(Inbox {
+        inbox: Mutex::new(tx)
+    }).unwrap();
+
+}
+
+struct Chat {
+    id: u32,
+    token: String,
+}
+
+impl Chat {
+    pub fn new(id: u32, token: String) -> Chat {
+        Chat {
+            id,
+            token
+        }
+    }
+
+    pub fn send_message(&self, text: String) {
+        let params = [("chat_id", &format!("{}", self.id)), ("text", &text)];
+        let client = reqwest::Client::new();
+        let res = client.post(&*format!("https://api.telegram.org/bot{}/sendMessage", self.token))
+            .form(&params)
+            .send();
+    }
+}
+
+fn bot(rx: Receiver<String>) {
+    loop {
+        let msg = rx.recv().unwrap();
+        let token = env::var("TELEGRAM_BOT_TOKEN").unwrap();
+        let chat = Chat::new(142664361, token);
+        chat.send_message(msg);
+    }
+}
 
 fn main() {
-    let mut core = Core::new().unwrap();
+    let (tx, rx) = channel();
 
-    let token = env::var("TELEGRAM_BOT_TOKEN").unwrap();
-    let api = Api::configure(token).build(core.handle()).unwrap();
-
-    // Fetch new updates via long poll method
-    let future = api.stream().for_each(|update| {
-
-        // If the received update contains a new message...
-        if let UpdateKind::Message(message) = update.kind {
-
-            if let MessageKind::Text {ref data, ..} = message.kind {
-                // Print received text message to stdout.
-                println!("<{}>: {}", &message.from.first_name, data);
-
-                // Answer message with "Hi".
-                api.spawn(message.text_reply(
-                    format!("Hi, {}! You just wrote '{}'", &message.from.first_name, data)
-                ));
-            }
-        }
-
-        Ok(())
+    let child1 = thread::spawn(move || {
+        inbox(tx);
     });
+    println!("started inbox");
 
-    core.run(future).unwrap();
+    let child2 = thread::spawn(move || {
+        bot(rx);
+    });
+    println!("started bot");
+
+    child1.join().unwrap();
+    child2.join().unwrap();
 }
